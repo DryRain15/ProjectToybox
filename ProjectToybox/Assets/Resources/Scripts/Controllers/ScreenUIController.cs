6991c8d7888778a860c5cd58363595f53c4bfe0d
DryRain <kachirjsdn@gmail.com>
2021-03-23 18:15:41 +0900

    ScreenUIController에 ScreenFade 기능 추가

--- ProjectToybox/Assets/Resources/Scripts/Controllers/ScreenUIController.cs
+++ ProjectToybox/Assets/Resources/Scripts/Controllers/ScreenUIController.cs
@@ -2,11 +2,13 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScreenUIController : MonoBehaviour
 {
     public static ScreenUIController Instance;
     private Canvas _canvas;
+    private RawImage _fade;
 
     private void Awake()
     {
@@ -14,10 +16,35 @@ public class ScreenUIController : MonoBehaviour
         else Instance = this;
 
         _canvas = GetComponent<Canvas>();
+
+        _fade = transform.Find("Fade")?.GetComponent<RawImage>();
+        if (_fade == null)
+        {
+            _fade = new GameObject("Fade").AddComponent<RawImage>();
+            _fade.transform.SetParent(transform);
+        }
     }
 
     private void Start()
     {
         _canvas.worldCamera = Camera.main;
+        _fade.color = Color.black;
+        ScreenFadeCall(new Color(0, 0, 0, 0), 1f);
+    }
+
+    public void ScreenFadeCall(Color color, float duration)
+    {
+        CoroutineManager.Instance.StartCoroutineCall(ScreenFadeRoutine(_fade.color, color, duration));
+    }
+
+    private IEnumerator ScreenFadeRoutine(Color from, Color to, float duration)
+    {
+        float innerTimer = 0f;
+        while (innerTimer < duration)
+        {
+            _fade.color = Color.Lerp(from, to, innerTimer / duration);
+            innerTimer += Time.deltaTime;
+            yield return null;
+        }
     }
 }
